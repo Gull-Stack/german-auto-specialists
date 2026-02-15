7f67a52a01920f0f3f9b6b01d57945e37c12531a
Bryce Morgan <brycedmorgan@gmail.com>
2026-02-15 11:49:41 -0700

    Convert to 11ty with shared header/footer template

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  // Pass through static assets
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/vercel.json");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
