376e6ff5781a5a203d5dae2d1532f2fe2397b6d7
Bryce Morgan <brycedmorgan@gmail.com>
2026-02-16 13:00:51 -0700

    SEO: Fix URLs to www, add sitemap/robots passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/favicon.svg");
   eleventyConfig.addPassthroughCopy("src/vercel.json");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
 
   return {
     dir: {
